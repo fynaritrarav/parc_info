a200b3050bd61561246fa6fcf02bdeef86c4a578
fynaritrarav <finaritrarav4@gmail.com>
2025-07-09 11:42:25 +0200

    update htaccess, create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require_once './configuration/config.php';
+// require_once CORE . 'Router.php';
+// require_once CORE . 'Database.php';
+
+// require_once CORE . 'Controller.php';
+// require_once CORE . 'Model.php';
+
+require_once 'autoload.php';
+spl_autoload_register('autoload');
+
+require_once ROUTER ;
+
